d8b1019848ca04f009946535cf6a1ad6c7c01af9
mitra999 <57156351+mitra999@users.noreply.github.com>
2021-09-25 02:43:07 -0400

    Update CSV_binder.R

--- CSV_binder.R
+++ CSV_binder.R
@@ -4,10 +4,10 @@ library(data.table)
 # make a List of  all csv files in directory
 filenames <- list.files("CSV_Files", pattern="*.csv", full.names=TRUE)
 
-# read and combine data frame by rows 
+# read and combine data frame by rows the write the combined file
 data <- rbindlist(lapply(filenames,fread))
 write.csv(list.DFs,"All_CSVs_rbind.csv", row.names = FALSE)
 
-# combine data frame by columns 
+# combine data frame by columns and write the combined file
 list.DFs <- lapply(filenames,fread)
 write.csv(list.DFs,"All_CSVs_cbind.csv", row.names = FALSE)
